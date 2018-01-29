c5882fb80bab834e54897238bfb78add9107acd7
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-29 22:52:01 +0530

    added two component productlist and product desc

--- productpricecomparison/src/app/app.module.ts
+++ productpricecomparison/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductdescComponent } from './productlist/productdesc/productdesc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent,
+    ProductdescComponent
   ],
   imports: [
     BrowserModule
